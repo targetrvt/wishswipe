dc83dd2c9bfdc77f75a70952a2c0d5535a4c7495
targetrvt <A240350DF@rvt.lv>
2025-10-27 00:03:50 +0200

    2 logos for light mode and dark mode

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,5 +1,14 @@
-<img 
-    src="{{ asset('images/wishSwipe_logo2.png') }}" 
-    alt="WishSwipe Logo"
-    class="h-16"
-/>
+<div x-data="{ isDark: false }" x-init="isDark = document.documentElement.classList.contains('dark') || document.body.classList.contains('dark'); setInterval(() => { isDark = document.documentElement.classList.contains('dark') || document.body.classList.contains('dark'); }, 100)">
+    <img 
+        x-show="!isDark"
+        src="{{ asset('images/wishswipe_logo_black.png') }}" 
+        alt="WishSwipe Logo"
+        class="h-16"
+    />
+    <img 
+        x-show="isDark"
+        src="{{ asset('images/wishSwipe_logo2.png') }}" 
+        alt="WishSwipe Logo"
+        class="h-16"
+    />
+</div>
